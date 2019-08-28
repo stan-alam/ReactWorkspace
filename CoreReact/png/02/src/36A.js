d769c9a65600d08caa6b28a16acf97b66fe1947b
Stanislaw Alam <stan.alam@gmail.com>
2019-08-28 00:36:08 +0000

    added codeblocks

---
+++ CoreReact/png/02/src/36A.js
@@ -0,0 +1,19 @@
+ReactDOM.render(
+  React.createElement(
+    'div',
+      null,
+      React.createElement(HelloMultiverses, {
+        id: 'Vue',
+        frameworkName: 'Vue.js',
+        title: 'popular among front-end devs'}),
+      React.createElement(HelloMultiverses, {
+        id: 'sailJS',
+        frameworkName: 'sail.js',
+        title: 'not too shabby'}),
+      React.createElement(HelloMultiverses, {
+        id: 'vanilla',
+        frameworkName: 'vanilla.js',
+        title: 'THE BEST!'})
+    ),
+    document.getElementById('content')
+  )
