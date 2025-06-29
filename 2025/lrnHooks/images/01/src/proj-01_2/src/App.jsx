1a8ba48ad70f21ec128b2b4bc00a00ff64eba5cd
stan alam <stan.alam@gmail.com>
2025-06-29 13:39:59 -0400

    added constructor to call super props

--- 2025/lrnHooks/images/01/src/proj-01_2/src/App.jsx
+++ 2025/lrnHooks/images/01/src/proj-01_2/src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export class App extends React.Component {
+    /* here you define a constructor method -- where you will init the state object--at this time will be
+     * an empty string object
+     * you need to make sure to call super(props) as to insure/let the React.Component Constructor know about
+     * the props object */
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    constructor(props) {
+        super(props)
+        this.state = { name: '' }
+    }
 }
-
-export default App
