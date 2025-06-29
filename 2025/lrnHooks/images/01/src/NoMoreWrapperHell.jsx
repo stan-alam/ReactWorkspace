d370641bf2f2253605c9d64479da76a76a9e35eb
stanislaw alam <stan.alam@gmail.com>
2025-06-29 11:24:27 -0400

    added non functional NoLongerWrapperHell

---
+++ 2025/lrnHooks/images/01/src/NoMoreWrapperHell.jsx
@@ -0,0 +1,5 @@
+const user = userContext(AuthContext)
+const language = useContext(LanguageContext)
+const status = userContext(StatusContext)
+const theme = useContext(ThemeContext)
+// non functional
