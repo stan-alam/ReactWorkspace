c9b1c9cd77dfb28c60128df48f129cade0ac27ee
stan alam <stan.alam@gmail.com>
2025-06-29 16:07:31 -0400

    added usr interface - fixed typo in closing tag

--- 2025/lrnHooks/images/01/src/proj-01_3/src/App.jsx
+++ 2025/lrnHooks/images/01/src/proj-01_3/src/App.jsx
@@ -1,28 +1,28 @@
 import { useState } from 'react'
 
 export function App() {
-/*
- * remember hooks are just js functions -- meaning you can use hooks from function components
- * like any other JS function
- * you can use hooks in any function component
- *
- * useState is a hook that allows you to add state to your function components
- * useState returns an array with two elements:
- * 1. the current state value
- * 2. a function to update the state value
- */
-// to use sate via Hooks  -- call useState with the initial state value - it will teurn an array with two elements
+    /*
+     * remember hooks are just js functions -- meaning you can use hooks from function components
+     * like any other JS function
+     * you can use hooks in any function component
+     *
+     * useState is a hook that allows you to add state to your function components
+     * useState returns an array with two elements:
+     * 1. the current state value
+     * 2. a function to update the state value
+     */
+    // to use sate via Hooks  -- call useState with the initial state value - it will teurn an array with two elements
 
-const [name, setName] = useState('')
+    const [name, setName] = useState('')
 
-function handleChange(evt) {
-    setName(evt.target.value)
-}
+    function handleChange(evt) {
+        setName(evt.target.value)
+    }
 
-return (
-    <div>
-        <h1><Hola! what is your name: {name}/h1>
-        <input type='text' value={name} onChange={handleChange} />
-    </div>
+    return (
+        <div>
+            <h1>Hola! what is your name: {name}</h1>
+            <input type='text' value={name} onChange={handleChange} />
+        </div>
     )
 }
